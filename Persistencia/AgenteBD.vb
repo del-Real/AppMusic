b3747332abd5c19e273921bed4ba92d4c266c8ac
Alberto del Real <66703149+del-Real@users.noreply.github.com>
2023-03-28 18:34:38 +0200

    agenteBD updated

--- Persistencia/AgenteBD.vb
+++ Persistencia/AgenteBD.vb
@@ -1,13 +1,11 @@
 ﻿Public Class AgenteBD
 
     Private Shared _instancia As AgenteBD
-    Private Shared conexion As OleDb.OleDbConnection
-    Private Const cadenaConexionBase As String = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source="
-    Private Shared cadenaConexion As String
+    Private Shared conexion As MySql.Data.MySqlClient.MySqlConnection
+    Private Const cadenaConexion As String = "server=localhost;database=personas;uid=root;pwd=password"
 
     Private Sub New()
-        AgenteBD.conexion = New OleDb.OleDbConnection(AgenteBD.cadenaConexion)
-        AgenteBD.conexion.Open()
+        AgenteBD.conexion = New MySql.Data.MySqlClient.MySqlConnection(AgenteBD.cadenaConexion)
     End Sub
 
     Public Shared Function ObtenerAgente() As AgenteBD
@@ -17,23 +15,18 @@
         Return AgenteBD._instancia
     End Function
 
-    Public Shared Function ObtenerAgente(ruta As String) As AgenteBD
-        AgenteBD.cadenaConexion = AgenteBD.cadenaConexionBase & ruta
-        Return AgenteBD.ObtenerAgente
-    End Function
-
     Public Function Leer(sql As String) As Collection
         Dim result As New Collection
         Dim fila As Collection
         Dim i As Integer
-        Dim reader As OleDb.OleDbDataReader 'Almacena toda la información de la consulta'
-        Dim com As New OleDb.OleDbCommand(sql, AgenteBD.conexion)
+        Dim reader As MySql.Data.MySqlClient.MySqlDataReader
+        Dim com As New MySql.Data.MySqlClient.MySqlCommand(sql, AgenteBD.conexion)
         Conectar()
         reader = com.ExecuteReader
         While reader.Read
             fila = New Collection
             For i = 0 To reader.FieldCount - 1
-                fila.Add(reader(i).ToString) 'i número de columnas de las cuales queremos leer un valor'
+                fila.Add(reader(i).ToString)
             Next
             result.Add(fila)
         End While
@@ -42,10 +35,10 @@
     End Function
 
     Public Function Modificar(sql As String) As Integer
-        Dim com As New OleDb.OleDbCommand(sql, AgenteBD.conexion)
+        Dim com As New MySql.Data.MySqlClient.MySqlCommand(sql, AgenteBD.conexion)
         Dim result As Integer
         Conectar()
-        result = com.ExecuteNonQuery 'Devuelve el número de filas afectadas'
+        result = com.ExecuteNonQuery
         Desconectar()
         Return result
     End Function
